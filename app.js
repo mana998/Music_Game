2911325dbfe83407ad860e7473d334f9556bf015
Marianna <mana998@azet.sk>
2021-04-30 21:16:24 +0200

    Added initial client/server setup

--- app.js
+++ app.js
@@ -6,9 +6,21 @@ app.use("/static", express.static('./static/'));
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+const FRAME_RATE = 30;
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 })
+
+io.on("connection", (socket) => {
+    setInterval(() => {
+        server.emit('gameState', gameState)
+    }, 1000 / FRAME_RATE)
+})
+
+
+
+
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, (error) => {
